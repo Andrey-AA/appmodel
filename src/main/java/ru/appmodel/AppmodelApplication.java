6380497aa7587fbd55776857cb855987425a0301
Andrey <abramov@okap.ru>
2023-01-17 00:37:39 +0300

    first commit

---
+++ src/main/java/ru/appmodel/AppmodelApplication.java
@@ -0,0 +1,13 @@
+package ru.appmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/appmodel/AppmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.appmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
